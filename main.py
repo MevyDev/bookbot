8074efbc3a4c8a4eae4e65a689f2e008da58248f
MevyDev <mevydev@proton.me>
2025-05-27 00:29:06 +0200

    feat: add get_word_count function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 BOOKS_PATH = "books/"
 
 
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+
 def get_book_text(book_name):
     with open(BOOKS_PATH + book_name + ".txt", "r") as f:
         content = f.read()
@@ -10,10 +15,12 @@ def get_book_text(book_name):
 def main():
     print("Welcome to bookbot! The best book analyzer on the planet!")
     print()
+
     book = input("Enter the name of the book you want to analyze: ")
     book_text = get_book_text(book)
 
-    print(book_text)
+    word_count = get_word_count(book_text)
+    print(f"{word_count} words found in the document")
 
 
 if __name__ == "__main__":
