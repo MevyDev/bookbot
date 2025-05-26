6aab72d6ed37fa2e7d5c1e22d394bd85d41a0d7c
MevyDev <mevydev@proton.me>
2025-05-27 00:04:15 +0200

    feat: add get_book_text function

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 BOOKS_PATH = "books/"
 
 
+def get_book_text(book_name):
+    with open(BOOKS_PATH + book_name, "r") as f:
+        content = f.read()
+        return content
+
+
 def main():
-    pass
+    book_name = input()
+    print(get_book_text(book_name))
 
 
 if __name__ == "__main__":
